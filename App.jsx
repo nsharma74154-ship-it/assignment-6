aa3ca50e53e9c210ac171d3fcb46cfed42663317
nsharma74154-ship-it <nsharma74154@gmail.com>
2025-11-02 23:33:04 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,19 @@
+
+import './App.css'
+import FeedbackForm from './components/FeedbackForm';
+import Header from './components/Header';
+
+function App() {
+  
+
+  return (
+    <>
+      <div>
+        <Header/>
+        <FeedbackForm/>
+      </div>
+    </>
+  )
+}
+
+export default App;
